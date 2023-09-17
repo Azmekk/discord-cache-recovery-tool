2f40254230935749df4eb1d9f9fe534e24400ea2
Martin Yordanov <martin.jordanov22@gmail.com>
2023-09-17 23:01:38 +0300

    Added a build script and changed code so that console doesn't instantly close

---
+++ src/build_script.ps1
@@ -0,0 +1,23 @@
+echo "Setting the target architecture to amd64"
+$env:GOARCH = "amd64"
+
+echo "Setting the target operating system to windows"
+$env:GOOS = "windows"
+echo "Building for windows x86-64"
+go build -o ../bin/recoverdiscordcache_windows.exe
+
+echo "Setting the target operating system to linux"
+$env:GOOS = "linux"
+echo "Building for linux x86-64"
+go build -o ../bin/recoverdiscordcache_linux
+
+echo "Setting the target operating system to darwin (macOS)"
+$env:GOOS = "darwin"
+echo "Building for mac x86-64"
+go build -o ../bin/recoverdiscordcache_mac
+
+echo "Resetting the environment variables"
+Remove-Item Env:\GOARCH
+Remove-Item Env:\GOOS
+
+echo "Script execution completed."
--- src/main.go
+++ src/main.go
@@ -25,4 +25,6 @@ func main() {
 	}
 
 	fmt.Println("Done!")
+	fmt.Println("You can close the window or press enter to exit...")
+	fmt.Scanln()
 }
